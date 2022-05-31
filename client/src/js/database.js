f7cab10f7cecc0094677a2b34a6d33180e103274
Bakary Sylla <syllabakary2002@gmail.com>
2022-05-31 15:17:59 -0700

    debug okay

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,10 +14,10 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.error("Post to the database");
+  console.log("Post to the database");
 
   // Create a connection to the database database and version we want to use.
-  const jateDb = awaitopenDB("jate", 1);
+  const jateDb = await openDB("jate", 1);
 
   // Create a new transaction and specify the database and data privileges.
   const tx = jateDb.transaction("jate", "readwrite");
